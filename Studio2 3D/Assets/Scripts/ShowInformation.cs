26aec34e5510b8afd634d47e3288f17d8d33e68b
Dyson <dysonkui@gmail.com>
2024-11-14 00:11:47 +0400

    Added panel information for potions

--- Studio2 3D/Assets/Scripts/ShowInformation.cs
+++ Studio2 3D/Assets/Scripts/ShowInformation.cs
@@ -1,20 +1,43 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class ShowInformation : MonoBehaviour
 {
-    public GameObject informationPanel;
+    public GameObject redInformationPanel;
+    public GameObject blueInformationPanel;
+    public GameObject yellowInformationPanel;
+
     private void OnMouseOver()
     {
-        informationPanel.SetActive(true);
+        if (this.GameObject().name == "potion_red")
+        {
+            redInformationPanel.SetActive(true);
+            blueInformationPanel.SetActive(false);
+            yellowInformationPanel.SetActive(false);
+        }
+        if (this.GameObject().name == "potion_blue")
+        {
+            blueInformationPanel.SetActive(true);
+            redInformationPanel.SetActive(false);
+            yellowInformationPanel.SetActive(false);
+        }
+        if (this.GameObject().name == "potion_yellow")
+        {
+            yellowInformationPanel.SetActive(true);
+            blueInformationPanel.SetActive(false);
+            redInformationPanel.SetActive(false);
+        }
     }
 
     private void OnMouseExit()
-    {
-        informationPanel.SetActive(false);
+    { 
+        redInformationPanel.SetActive(false);
+        blueInformationPanel.SetActive(false);
+        yellowInformationPanel.SetActive(false);
     }
 }
