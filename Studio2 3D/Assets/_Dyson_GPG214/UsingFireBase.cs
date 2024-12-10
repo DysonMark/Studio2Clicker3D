bdf0218c9aec6c0c6390b007d9a874dd5a55fbbd
Dyson <dysonkui@gmail.com>
2024-12-10 13:03:43 +0400

    Can now use extensions for firebase

--- Studio2 3D/Assets/_Dyson_GPG214/UsingFireBase.cs
+++ Studio2 3D/Assets/_Dyson_GPG214/UsingFireBase.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Firebase;
+using Firebase.Extensions;
+using Firebase.Storage;
 
 namespace SAE.GPG214.Dyson.Storage
 {
