bcaf901777af7da581d1e3cf563230a6b5adb399
Dyson <dysonkui@gmail.com>
2024-12-10 12:53:00 +0400

    Tracking large file with lfs

---
+++ Studio2 3D/Assets/_Dyson_GPG214/UsingFireBase.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SAE.GPG214.Dyson.Storage
+{
+    public class UsingFireBase : MonoBehaviour
+    {
+        private FirebaseStorage storage;
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+            storage = FirebaseStorage.GetInstance("gs://gpg214-dyson.firebasestorage.app");
+
+            var audioReference = storage.GetReference("Shadowfell Combat Dark Combat Music.mp3");
+            audioReference.GetDownloadUrlAsync().ContinueWithOnMainThread(task =>
+            {
+                var path = task.Result.ToString();
+                StartCoroutine(DownloadAudio(path));
+            });
+            // var audio = UnityWebRequestMultimedia.GetAudioClip(audioReference.);
+        }
+
+        private string DownloadAudio(string path)
+        {
+            Debug.Log("path:" + path);
+            return path;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }    
+}
