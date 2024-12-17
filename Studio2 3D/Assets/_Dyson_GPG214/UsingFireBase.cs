5cd29fbdd774b632ae0b4dc31623f636af8fa62f
Dyson <dysonkui@gmail.com>
2024-12-18 02:34:50 +0400

    Can download files from firebase, update and use it

--- Studio2 3D/Assets/_Dyson_GPG214/UsingFireBase.cs
+++ Studio2 3D/Assets/_Dyson_GPG214/UsingFireBase.cs
@@ -7,6 +7,10 @@ using Firebase;
 using Firebase.Extensions;
 using Firebase.Storage;
 using UnityEngine.Networking;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine.WSA;
+using Application = UnityEngine.Application;
 
 namespace SAE.GPG214.Dyson.Storage
 {
@@ -66,13 +70,14 @@ namespace SAE.GPG214.Dyson.Storage
 
         [SerializeField] bool simulateNoInternet;
 
-        List<FileData> storageBucketFileMetaDeta = new List<FileData>();
+        [SerializeField] List<FileData> storageBucketFileMetaDeta = new List<FileData>();
 
         IEnumerator Start()
         {
             if (Application.internetReachability == NetworkReachability.NotReachable || simulateNoInternet)
             {
                 Debug.LogError("No internet");
+                yield break;
             }
 
             GetFirebaseInstance();
@@ -117,17 +122,6 @@ namespace SAE.GPG214.Dyson.Storage
                 Debug.Log("Storage bucket not found");
             }
         }
-
-        IEnumerator DownloadFiles()
-        {
-            yield return null;
-        }
-
-        private bool IsFileUpToDate(FileInfo localFile, FileData metaData)
-        {
-            return true;
-        }
-
         IEnumerator GetAllFilesInBucket()
         {
             for (int i = 0; i < allFilesInBucket.Count; i++)
@@ -145,14 +139,131 @@ namespace SAE.GPG214.Dyson.Storage
             }
             yield return null;
         }
-
+        
         IEnumerator GetFileMetaData(StorageReference fileToCheck)
         {
+            Task<StorageMetadata> fileToCheckMetaData = fileToCheck.GetMetadataAsync();
+
+            while (fileToCheckMetaData != null && !fileToCheckMetaData.IsCompleted)
+            {
+                Debug.Log("Getting file meta data " + fileToCheck.Name);
+                yield return null;
+            }
+
+            StorageMetadata metadata = fileToCheckMetaData.Result;
+
+            if (metadata != null)
+            {
+                FileData newFile = new FileData();
+
+                newFile.fileName = metadata.Name;
+                newFile.dateCreated = metadata.CreationTimeMillis;
+                newFile.dateLastModified = metadata.UpdatedTimeMillis;
+                newFile.dateCreatedString = metadata.CreationTimeMillis.ToUniversalTime().ToString();
+                newFile.dateModifiedString = metadata.UpdatedTimeMillis.ToUniversalTime().ToString();
+                newFile.fileSize = metadata.SizeBytes;
+                //bool folderExists = File.Exists(destinationFolderPath);
+                if (File.Exists(destinationFolderPath))
+                    newFile.fileDestination = Path.Combine(destinationFolderPath, metadata.Name);
+                storageBucketFileMetaDeta.Add(newFile);
+            }
             yield return null;
+        }
+        IEnumerator DownloadFiles()
+        {
+            for (int i = 0; i < storageBucketFileMetaDeta.Count; i++)
+            {
+                // check to see if there's already a local file
+                bool fileExists = File.Exists(storageBucketFileMetaDeta[i].fileDestination);
+
+                if (fileExists)
+                {
+                    //check to see if it's up to date
+
+                    if (!IsFileUpToDate(new FileInfo(storageBucketFileMetaDeta[i].fileDestination),
+                            storageBucketFileMetaDeta[i]))
+                    {
+                        //if it's not, delete the local file
+                        File.Delete(storageBucketFileMetaDeta[i].fileDestination);
+                        Debug.Log("Deleting file" + storageBucketFileMetaDeta[i].fileName);
+                        fileExists = false;
+                    }
+                    // if it is don't do anything
+                }
+
+                if (!fileExists)
+                { 
+                    //download the file and wait for it to happen
+                    yield return DownloadFile(storageBucket.Child(storageBucketFileMetaDeta[i].fileName));
+                }
+            }
+            yield return null;
+        }
+
+        private bool IsFileUpToDate(FileInfo localFile, FileData metaData)
+        {
+            bool isUpToDate = true;
+            bool dateIsNewer = false;
+            bool fileSizeIsDifferent = false;
+
+            DateTime metaDataTimeUTC = metaData.dateLastModified.ToUniversalTime();
+            DateTime localFileTimeUTC = localFile.LastWriteTime.ToUniversalTime();
+
+            metaDataTimeUTC = new DateTime(metaDataTimeUTC.Year, metaDataTimeUTC.Month, metaDataTimeUTC.Day,
+                metaDataTimeUTC.Hour, metaDataTimeUTC.Minute, metaDataTimeUTC.Second);
+            localFileTimeUTC = new DateTime(localFileTimeUTC.Year, localFileTimeUTC.Month, localFileTimeUTC.Day,
+                localFileTimeUTC.Hour, localFileTimeUTC.Minute, localFileTimeUTC.Second);
+
+            dateIsNewer = DateTime.Compare(metaDataTimeUTC, localFileTimeUTC) > 0;
+            
+            fileSizeIsDifferent = localFile.Length != metaData.fileSize;
+
+            isUpToDate = !(dateIsNewer || fileSizeIsDifferent);
+            return isUpToDate;
         }
 
         IEnumerator DownloadFile(StorageReference fileToDownload)
         {
+            Task<Uri> uri = fileToDownload.GetDownloadUrlAsync();
+
+            while (!uri.IsCompleted)
+            {
+                Debug.Log("Getting URI Data " + fileToDownload.Name);
+                yield return null;
+            }
+            // once we get to here we have the info on where it's located online
+
+            UnityWebRequest www = new UnityWebRequest(uri.Result);
+            www.downloadHandler = new DownloadHandlerBuffer();
+
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log("Request successful");
+                byte[] resultData = www.downloadHandler.data;
+
+                while (www.downloadProgress < 1)
+                {
+                    Debug.Log("Downloading File " + fileToDownload.Name + (www.downloadProgress * 100) + "%");
+                    yield return null;
+                }
+
+                string destinationPath = Path.Combine(destinationFolderPath, fileToDownload.Name);
+
+                Task writeFile = File.WriteAllBytesAsync(destinationPath, resultData);
+
+                while (!writeFile.IsCompleted)
+                {
+                    Debug.Log("Writing file data please" + fileToDownload.Name);
+                    yield return null;
+                }
+                Debug.Log("Download completed!");
+            }
             yield return null;  
         }
     }    
